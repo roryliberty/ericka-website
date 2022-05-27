91f993bb4326fcf45f07247d50faf1a622da57a0
Rory Liberty <roryliberty@gmail.com>
2022-05-27 13:46:26 -0500

    Added nice-words to ericka-container

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
+
+
 /* CREATE ELEMENTS */
-let sourceLarge = document.createElement('source');
-let sourceMedium = document.createElement('source');
-let sourceSmall = document.createElement('source');
-let imgEl = document.createElement('img');
+let serviceSourceLarge = document.createElement('source');
+let serviceSourceMedium = document.createElement('source');
+let serviceSourceSmall = document.createElement('source');
+let serviceImgEl = document.createElement('img');
+// let erickaImgEl = document.createElement('img');
 
 /* SET ATTRIBUTES */
-sourceLarge.setAttribute('srcset', 'https://www.picsum.photos/750/250');
-sourceLarge.setAttribute('media', '(min-width: 1140px)');
-sourceMedium.setAttribute('srcset', 'https://www.picsum.photos/650/217');
-sourceMedium.setAttribute('media', '(min-width: 992px)');
-sourceSmall.setAttribute('srcset', 'https://www.picsum.photos/475/158');
-sourceSmall.setAttribute('media', '(min-width: 768px)');
-imgEl.setAttribute('src', 'https://www.picsum.photos/253/106');
-imgEl.setAttribute('alt', 'Cage Rage');
+serviceSourceLarge.setAttribute('srcset', 'https://www.picsum.photos/750/250');
+serviceSourceLarge.setAttribute('media', '(min-width: 1140px)');
+serviceSourceMedium.setAttribute('srcset', 'https://www.picsum.photos/650/217');
+serviceSourceMedium.setAttribute('media', '(min-width: 992px)');
+serviceSourceSmall.setAttribute('srcset', 'https://www.picsum.photos/475/158');
+serviceSourceSmall.setAttribute('media', '(min-width: 768px)');
+serviceImgEl.setAttribute('src', 'https://www.picsum.photos/253/106');
+serviceImgEl.setAttribute('alt', 'Cage Rage');
+erickaImgEl.setAttribute('src', 'https://www.placecage.com/300/300');
+// erickaImgEl.setAttribute('alt', 'Ericka');
+// erickaImgEl.setAttribute('id', 'img-ericka');
 
 /* ARRAY OF ELEMENTS WITH ATTRIBUTES SET */
-let elems =  [sourceLarge, sourceMedium, sourceSmall, imgEl];
+let elems =  [serviceSourceLarge, serviceSourceMedium, serviceSourceSmall, serviceImgEl];
 
 /* GRAB SERVICE-IMAGE CLASS ELEMENTS */
 let serviceImage = document.getElementsByClassName('service-image');
+// let erickaContainer = document.getElementById('ericka-container-fix');
 
 /* CREATE DOCUMENT FRAGMENT */
 let fragment = document.createDocumentFragment();
@@ -32,3 +39,5 @@ for (let e = 0; e < elems.length; e++) {
 for (let i = 0; i < serviceImage.length; i++) {
     serviceImage[i].appendChild(fragment.cloneNode(true));
 }
+
+// erickaContainer.appendChild(erickaImgEl);
