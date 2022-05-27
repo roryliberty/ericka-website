f2d3b2af391b4521f718db70e3db04114e95697e
Rory Liberty <roryliberty@gmail.com>
2022-05-27 15:08:07 -0500

    Converted most images to webP Removed jpg and png images

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-
-
 /* CREATE ELEMENTS */
 let serviceSourceLarge = document.createElement('source');
 let serviceSourceMedium = document.createElement('source');
 let serviceSourceSmall = document.createElement('source');
 let serviceImgEl = document.createElement('img');
-// let erickaImgEl = document.createElement('img');
 
 /* SET ATTRIBUTES */
 serviceSourceLarge.setAttribute('srcset', 'https://www.picsum.photos/750/250');
 serviceSourceLarge.setAttribute('media', '(min-width: 1140px)');
+serviceSourceLarge.setAttribute('width', '750');
+serviceSourceLarge.setAttribute('height', '250');
 serviceSourceMedium.setAttribute('srcset', 'https://www.picsum.photos/650/217');
 serviceSourceMedium.setAttribute('media', '(min-width: 992px)');
+serviceSourceMedium.setAttribute('width', '650');
+serviceSourceMedium.setAttribute('height', '217');
 serviceSourceSmall.setAttribute('srcset', 'https://www.picsum.photos/475/158');
 serviceSourceSmall.setAttribute('media', '(min-width: 768px)');
+serviceSourceSmall.setAttribute('width', '475');
+serviceSourceSmall.setAttribute('height', '158');
 serviceImgEl.setAttribute('src', 'https://www.picsum.photos/253/106');
+serviceImgEl.setAttribute('width', '253');
+serviceImgEl.setAttribute('height', '106');
 serviceImgEl.setAttribute('alt', 'Cage Rage');
-erickaImgEl.setAttribute('src', 'https://www.placecage.com/300/300');
-// erickaImgEl.setAttribute('alt', 'Ericka');
-// erickaImgEl.setAttribute('id', 'img-ericka');
 
 /* ARRAY OF ELEMENTS WITH ATTRIBUTES SET */
 let elems =  [serviceSourceLarge, serviceSourceMedium, serviceSourceSmall, serviceImgEl];
 
 /* GRAB SERVICE-IMAGE CLASS ELEMENTS */
 let serviceImage = document.getElementsByClassName('service-image');
-// let erickaContainer = document.getElementById('ericka-container-fix');
 
 /* CREATE DOCUMENT FRAGMENT */
 let fragment = document.createDocumentFragment();
@@ -39,5 +40,3 @@ for (let e = 0; e < elems.length; e++) {
 for (let i = 0; i < serviceImage.length; i++) {
     serviceImage[i].appendChild(fragment.cloneNode(true));
 }
-
-// erickaContainer.appendChild(erickaImgEl);
