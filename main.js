d18654a8e648493f982eb96399d42f774207afb1
Rory Liberty <roryliberty@gmail.com>
2022-06-01 16:16:36 -0500

    Finished 1366x768
    Took out several other media queries.

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 /* CREATE ELEMENTS */
-let serviceSourceLarge = document.createElement('source');
+// let serviceSourceLarge = document.createElement('source');
 let serviceSourceMedium = document.createElement('source');
 let serviceSourceSmall = document.createElement('source');
 let serviceImgEl = document.createElement('img');
 
 /* SET ATTRIBUTES */
-serviceSourceLarge.setAttribute('srcset', 'https://www.picsum.photos/750/250');
-serviceSourceLarge.setAttribute('media', '(min-width: 1140px)');
-serviceSourceLarge.setAttribute('width', '750');
-serviceSourceLarge.setAttribute('height', '250');
-serviceSourceMedium.setAttribute('srcset', 'https://www.picsum.photos/650/217');
-serviceSourceMedium.setAttribute('media', '(min-width: 992px)');
-serviceSourceMedium.setAttribute('width', '650');
-serviceSourceMedium.setAttribute('height', '217');
+// serviceSourceLarge.setAttribute('srcset', 'https://www.picsum.photos/750/250');
+// serviceSourceLarge.setAttribute('media', '(min-width: 1140px)');
+// serviceSourceLarge.setAttribute('width', '750');
+// serviceSourceLarge.setAttribute('height', '250');
+serviceSourceMedium.setAttribute('srcset', 'https://www.picsum.photos/653/270');
+serviceSourceMedium.setAttribute('media', '(min-width: 1320px)');
+serviceSourceMedium.setAttribute('width', '653');
+serviceSourceMedium.setAttribute('height', '270');
 serviceSourceSmall.setAttribute('srcset', 'https://www.picsum.photos/475/158');
 serviceSourceSmall.setAttribute('media', '(min-width: 768px)');
 serviceSourceSmall.setAttribute('width', '475');
@@ -23,7 +23,7 @@ serviceImgEl.setAttribute('height', '106');
 serviceImgEl.setAttribute('alt', 'Cage Rage');
 
 /* ARRAY OF ELEMENTS WITH ATTRIBUTES SET */
-let elems =  [serviceSourceLarge, serviceSourceMedium, serviceSourceSmall, serviceImgEl];
+let elems =  [/*serviceSourceLarge,*/ serviceSourceMedium, serviceSourceSmall, serviceImgEl];
 
 /* GRAB SERVICE-IMAGE CLASS ELEMENTS */
 let serviceImage = document.getElementsByClassName('service-image');
